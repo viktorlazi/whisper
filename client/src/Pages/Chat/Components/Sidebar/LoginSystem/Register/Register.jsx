bfa788f75cb5f40695207fdf227e39bceab6fd73
Viktorlazi <viktorlazi010@gmail.com>
2021-06-24 18:56:18 +0200

    .

--- client/src/Pages/Chat/Components/Sidebar/LoginSystem/Register/Register.jsx
+++ client/src/Pages/Chat/Components/Sidebar/LoginSystem/Register/Register.jsx
@@ -7,7 +7,7 @@ function Register() {
   return <form id="register">
     <input value={store.username} placeholder='username' onChange={e=>store.setUsername(e.target.value)} type="text" />
     <input value={store.password} placeholder='password' onChange={e=>store.setPassword(e.target.value)} type="password" />
-    <input value={store.repeatPassword} placeholder='repeat password' onChange={e=>store.setPassword(e.target.value)} type="password" />
+    <input value={store.repeatPassword} placeholder='repeat password' onChange={e=>store.setRepeatPassword(e.target.value)} type="password" />
     <button onClick={e=>store.register(e)}>
       Register
     </button>
