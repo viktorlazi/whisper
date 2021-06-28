e60d109020497971198122b29765cdb8e36bbf0a
Viktorlazi <viktorlazi010@gmail.com>
2021-06-28 14:12:19 +0200

    wiki link

--- client/src/Pages/Chat/Components/Body/NoChat.jsx
+++ client/src/Pages/Chat/Components/Body/NoChat.jsx
@@ -9,6 +9,7 @@ export default function NoChat() {
 				<p>- Whisper cannot read your messages.</p>
 				<p>- Your messages are encrypted with E2EE.</p>
 				<p>- Whisper only stores your hashed password and contacts (optional).</p>
+				<p>- Learn mroe about encryption method: <a target="_blank" href="https://en.wikipedia.org/wiki/Diffie%E2%80%93Hellman_key_exchange">Diffie–Hellman key exchange</a></p>
 			</div>
 		</div>
 	)
